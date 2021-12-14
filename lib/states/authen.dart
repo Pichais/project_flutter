cd8491fda109b3c1a0f2b48cc463df13f6011ef1
Bank <yougvb@gmail.com>
2021-12-14 22:23:38 +0700

    Update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -11,6 +11,8 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
@@ -20,21 +22,95 @@ class _AuthenState extends State<Authen> {
           children: [
             BuildImage(size),
             buildAppName(),
+            buildUser(size),
+            buildPassword(size),
           ],
         ),
       ),
     );
   }
 
-  Row buildAppName() {
-    return Row( mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ShowTitle(
-                title: Myconstant.appName,
-                textStyle: Myconstant().h1style(),
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: Myconstant().h3style(),
+              labelText: 'User :',
+              prefixIcon: Icon(
+                Icons.account_circle,
+                color: Myconstant.dark,
               ),
-            ],
-          );
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Myconstant.dark),
+                  borderRadius: BorderRadius.circular(20)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Myconstant.light),
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    statusRedEye = !statusRedEye;
+                  });
+                },
+                icon: statusRedEye ? Icon(
+                  Icons.remove_red_eye,
+                  color: Myconstant.dark,
+                ): Icon(
+                  Icons.remove_red_eye_outlined,
+                  color: Myconstant.dark,
+                ), 
+              ),
+              labelStyle: Myconstant().h3style(),
+              labelText: 'Password :',
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Myconstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Myconstant.dark),
+                  borderRadius: BorderRadius.circular(20)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Myconstant.light),
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowTitle(
+          title: Myconstant.appName,
+          textStyle: Myconstant().h1style(),
+        ),
+      ],
+    );
   }
 
   Row BuildImage(double size) {
